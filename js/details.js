2bd4bf6b6925b6847d7cbeaaf7354797b1e43372
Andrea <andreacastsalazar@gmail.com>
2022-10-23 14:07:06 -0500

    details con API

--- js/details.js
+++ js/details.js
@@ -1,12 +1,19 @@
+let detailsC = document.getElementById("details");
 
-    let captur = location.search
-    let cap= new URLSearchParams(captur)
-    let otherId = parseInt(cap.get("id")) //convierto a un entero
-    let element = events.find((element) => element._id === otherId)
-    
-    let detailsC = document.getElementById("details")
+async function getId() {
+  let id = location.search.slice(4);
+  console.log(id);
+  let result = await fetch(`https://mind-hub.up.railway.app/amazing/${id}`);
+  console.log(result);
+  let resultApi = await result.json();
+  console.log(resultApi);
+  let event = resultApi.event;
+  printDetails(event);
+}
+getId();
 
-   detailsC.innerHTML = `
+function printDetails(element) {
+  detailsC.innerHTML = `
    <div class="card mb-3  " style="max-width: 800px; height: 402px;" >
     <div class="row g-0  ">
     <div class="col-md-6 " >
@@ -14,8 +21,12 @@
     </div>
     <div class="col-md-6 ">
       <div class="card-body  d-flex  flex-column align-items-center fs-5 text-white" style="background-color: #550053;  height: 400px;">
-        <h2 class="card-title d-flex justify-content-md-center">${element.name}</h2>
-        <p class="card-text d-flex d-flex align-items-end ">${element.description} </p>
+        <h2 class="card-title d-flex justify-content-md-center">${
+          element.name
+        }</h2>
+        <p class="card-text d-flex d-flex align-items-end ">${
+          element.description
+        } </p>
         <p>Date: ${element.date}</p>
         <p> Category : ${element.category}</p>
         <p>Place: ${element.place}</p>
@@ -26,4 +37,5 @@
     </div>
   </div>
   </div>
-    `
+    `;
+}
